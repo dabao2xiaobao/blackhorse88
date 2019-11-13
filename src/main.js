642cd3daab185c9aee72950abf2f9feaec875627
dabao2xiaobao <mimawowangle0.0>
2019-11-13 19:54:49 +0800

    项目初始化工作准备完毕

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import './styles/index.less'
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 new Vue({
