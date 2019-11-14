13b354084bc516b4f4121f95b6a665b4ec1def23
dabao2xiaobao <mimawowangle0.0>
2019-11-14 12:25:37 +0800

    axios下包及设置常态地址,axios共享给所有的实例应用

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import './styles/index.less'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 设置 axios的常态地址
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 new Vue({
